2f89478ce34a67498dc7e4c7d144282414188c9e
Fantomas42 <fantomas42@gmail.com>
2015-01-15 11:22:05 +0100

    Create the command

---
+++ zinnia_blogger/__init__.py
@@ -0,0 +1,8 @@
+"""blogger2zinnia"""
+__version__ = '1.0'
+__license__ = 'BSD License'
+
+__author__ = 'Fantomas42'
+__email__ = 'fantomas42@gmail.com'
+
+__url__ = 'https://github.com/django-blog-zinnia/blogger2zinnia'
